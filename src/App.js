00de221fae87b351e5a73e907052dd468f61458c
prathimakumari <prathimakumari3@gmail.com>
2023-04-08 18:23:06 +0530

    react-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+
+import { Box } from '@mui/material';
+
+import Header from './Components/header/Header';
+
+import Home from './Components/Home/Home';
+import Cart from './Components/cart/Cart'
+import DetailView from './Components/details/DetailView';
+import Shipping from "./Components/shipping/Shipping";
+import Context from "./context/Context";
+
+
+//=======================================function starts=======================================
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        hello
-        </a>
-      </header>
-    </div>
+    <Context>
+      <Router>
+        <Header />
+        <Box style={{ marginTop: 54 }}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/products/:id' element={<DetailView />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/shipping' element={<Shipping />} />
+          </Routes>
+        </Box>
+      </Router>
+    </Context>
   );
 }
 
 export default App;
+
